db5dc779ee1d69794c5ae3351ef633da6ba3d43e
Breon Knight <Breonknight7@gmail.com>
2016-02-21 21:52:36 -0800

    added controller definitions in users controller as well as a show page that shows username upon login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,22 +11,46 @@ class UsersController < ApplicationController
   end
 
   def create
-
+    @user = user_params
+    if @user.save
+      flash[:notice] = "Succesfully created account"
+      login(@user)
+      redirect_to @user
+    else
+      flash[:error] = @user.errors.full_messages.join(', ')
+      redirect_to new_user_path
   end
 
   def show
-
+    user_id = params[:id]
+    @user = User.find_by_name(user_id)
+    render :show
   end
 
   def edit
-
+    user_id = params[:id]
+    @user = User.find_by_name(user_id)
+    render :edit
   end
 
   def update
-
+    user_id = params[:id]
+    user = User.name(user_id)
+    user_edit_params = params.require(:user).permit(:first_name, :last_name, :twitch_name, :avatar)
+    # If we decide to let users update email address, email will be downcased before submission & update
+    user.update_attributes(user_edit_params)
+    redirect_to user_path(user)
   end
 
   def destroy
-
+    user_id = params[:id]
+    user = User.find_by_userName(user_id)
+    user.destroy
+    redirect_to root
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :name, :twitch_name, :email, :password, :avatar )
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ActiveRecord::Base
+  #for paperclip avatar uploading
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+  #allows username to be the path for user instead of id number
+  def to_param
+    name
+  end
+  #bcrypt safe password
+  has_secure_password
 end
