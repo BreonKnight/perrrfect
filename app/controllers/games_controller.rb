4264dba76f7fd53994d90fa716ad968282e00eed
Breon Knight <Breonknight7@gmail.com>
2016-02-22 01:01:56 -0800

    added controller logic for games

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,43 @@
 class GamesController < ApplicationController
+  def index
+    @games = Game.all
+    render :index
+  end
+
+  def new
+    @games = Game.new
+    render :new
+  end
+
+  def create
+    @game = Game.create(game_params)
+    if @game.save
+      flash[:notice] = "New Game has been added"
+      redirect_to @game
+    else
+      flash[:error] = @game.errors.full_messages.join(', ')
+      redirect_to new_game_path
+    end
+  end
+
+  def show
+    game_id = params[:id]
+    @game = find_by_title(game_id)
+    render :show
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  private
+
+    def game_params
+      params.require(:user).permit(:title, :avatar )
+    end
+
 end
