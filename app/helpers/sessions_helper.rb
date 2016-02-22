361f935a6ed929160fbcfe372e2b0f9b81cd7a77
Breon Knight <Breonknight7@gmail.com>
2016-02-21 21:59:50 -0800

    added logic for session helper and included it in applicationcontroller

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,23 @@
 module SessionsHelper
+
+  #logging user in by session
+  def login(user)
+    session[:user_id] = user.id
+    @current_user = user
+  end
+  #defining the current user by id upon logging in
+  def current_user
+    @current_user ||= User.find_by_id(session[:user_id])
+  end
+  #is the user logged in?
+  def logged_in?
+    if current_user == nil
+      redirect_to new_session_path
+    end
+  end
+  #session helper to logout
+  def logout
+    @current_user = session[:user_id] = nil
+  end
+  
 end
