6abde710fb8cef122b7d829884c73d4aba67181f
Breon Knight <Breonknight7@gmail.com>
2016-02-21 20:05:33 -0800

    ran migration to add paperclip to users model now avatar avaliable

--- db/migrate/20160222040336_add_avatar_columns_to_users.rb
+++ db/migrate/20160222040336_add_avatar_columns_to_users.rb
@@ -1,4 +1,9 @@
 class AddAvatarColumnsToUsers < ActiveRecord::Migration
-  def change
+  def up
+  add_attachment :users, :avatar
+  end
+
+  def down
+  remove_attachment :users, :avatar
   end
 end
