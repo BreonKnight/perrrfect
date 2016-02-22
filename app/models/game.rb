fd77db8bc23775845ff66a0ed297679ab78f1860
Breon Knight <Breonknight7@gmail.com>
2016-02-22 01:03:06 -0800

    added comment in game model

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,6 +2,7 @@ class Game < ActiveRecord::Base
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 
+  #setting game title to be the title name instead of number id number
   def to_param
     title
   end
