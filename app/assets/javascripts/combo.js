9665482a05bb5aabb3d5a783f5beda61c7ebc0d5
Breon Knight <Breonknight7@gmail.com>
2016-02-24 14:51:41 -0800

    getting alt image names for files

--- app/assets/javascripts/combo.js
+++ app/assets/javascripts/combo.js
@@ -2,9 +2,6 @@ angular.module('comboTime', ['ui.router', 'templates'])
   .controller('ComboMaker', [
     '$scope',
     function($scope){
-      var myEl = angular.element( document.querySelector( '#HK' ) );
-      // $scope.element = myEl
-      // $scope.test = 'Hello World'
-      // console.log(myEl)
+
     }
   ]);
